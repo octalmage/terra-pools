8700341d87abfa7fb2bdaf9a8abac771fcade8fb
Jason Stallings <jason@stallin.gs>
2022-05-09 23:23:36 -0500

    fix: Make domain more dynamic.

--- src/App.js
+++ src/App.js
@@ -8,25 +8,35 @@ const colors = ["#0088FE", "#FFBB28"];
 
 function App() {
   const [data, setData] = useState([]);
+  const [scale, setScale] = useState([49000000, 51000000]);
   const lcd = useLCDClient();
 
   useEffect(() => {
     const fetchPools = async () => {
       const delta = await lcd.market.poolDelta();
       const { base_pool } = await lcd.market.parameters();
-      const terraSide = base_pool.plus(delta);
-      const lunaSide = base_pool.pow(2).div(terraSide);
+      const microTerraSide = base_pool.plus(delta);
+      const micraLunaSide = base_pool.pow(2).div(microTerraSide);
+      
+      const terraSide = microTerraSide.times(0.000001).toNumber()
+      const lunaSide = micraLunaSide.times(0.000001).toNumber()
 
       setData([
         {
           name: "Stablecoins",
-          amt: terraSide.times(0.000001).toNumber(),
+          amt: terraSide,
         },
         {
           name: "LUNA",
-          amt: lunaSide.times(0.000001).toNumber(),
+          amt: lunaSide,
         },
       ]);
+
+      if (terraSide < 49000000 || lunaSide < 49000000) {
+        setScale([40000000, 60000000]);
+      } else {
+        setScale([49000000, 51000000]);
+      }
     };
 
     fetchPools();
@@ -41,11 +51,10 @@ function App() {
       <center>
         <h1>Terra Virtual Liquidity Pools</h1>
         <BarChart width={800} height={400} data={data}>
-          <Bar isAnimationActive={false} label={true} dataKey="amt">
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-            ))}
+          <Bar isAnimationActive={false} label={false} dataKey="amt">
+            {data.map((entry, index) => <Cell key={`cell-${index}`} fill={colors[index % 20]} />)}
             <LabelList dataKey="name" position="top" />
+            {/* <LabelList dataKey="amt" position="inside" fill="#000000" /> */}
           </Bar>
           <ReferenceLine
             isFront={false}
@@ -59,7 +68,7 @@ function App() {
             ticks={10}
             stroke="#000000"
             interval={0}
-            domain={[49000000, 51000000]}
+            domain={scale}
           />
         </BarChart>
       </center>
